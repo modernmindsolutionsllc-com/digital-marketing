fad8f9c93af4debaed2710f55b82b0f9c2ac6003
Subhabrata-backend <sruidas@modernmindsolutionsllc.com>
2026-05-02 18:13:13 +0530

    feat: wire React Router for admin portal routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import AdminLogin from "./components/AdminLogin";
+import PrivateRoute from "./components/PrivateRoute";
+import DashboardLayout from "./layouts/DashboardLayout";
+import LeadsDashboard from "./components/LeadsDashboard";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Public: Main website */}
+        <Route path="/" element={<App />} />
+
+        {/* Public: Admin login */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+
+        {/* Protected: Admin dashboard */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <DashboardLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard" element={<LeadsDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
